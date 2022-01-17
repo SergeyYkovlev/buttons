d468d0bfe592ac278f1e6d4d071eb94941557f92
SergeyYkovlev <yakovlevserg09@gmail.com>
2022-01-17 16:06:19 +0600

    wrap text in label on click

--- button/ViewController.swift
+++ button/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
-
+    
+    // Оутлет для текстового поя
+    @IBOutlet weak var someTextField: UITextField!
+    
+    // Оутлет для лейбла с результатом
+    @IBOutlet weak var result: UILabel!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    //Экшн для кнопки
+    
+    @IBAction func button(_ sender: Any) {
+        
+       
+        
+        result.text = someTextField.text
+    }
+    
+    
+    
+    
 }
+
 
